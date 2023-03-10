5b32be5b1865aff02b7ae665c8c5632a3c166499
Sepiro10 <126035782+Sepiro10@users.noreply.github.com>
2023-03-10 14:03:04 -0500

    Add files via upload

---
+++ modularidad/src/main/java/com/mycompany/modularidad/Modularidad.java
@@ -0,0 +1,21 @@
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.modularidad;
+
+/**
+ *
+ * @author sebas
+ */
+public class Modularidad {
+
+    public static void main(String[] args) {
+      Datos obj1 = new Datos();
+      obj1.leerDatos();
+      obj1.getAreaCirculo();
+      obj1.getAreaCuadrado();
+      obj1.imprimirResult();
+    }
+}
